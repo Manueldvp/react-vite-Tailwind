48c20612a3c12d1e874a0bb8d2c3c2e5f41d4466
manueldvp <manuelbyto@gmail.com>
2023-08-23 17:58:47 -0400

    Estructura de Carpetas

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../Myorder'
+import Myorders from '../Myorders'
+import NotFound from '../NotFound'
+import SingIn from '../SignIn'
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+      <div className='bg-gray-800' >
+       Hola
+      </div>
+    </>
+  )
+}
+
+export default App
