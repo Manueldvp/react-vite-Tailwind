8fcad6259ddb1ecaac5b6fd82f473676bd65d61f
manueldvp <manuelbyto@gmail.com>
2023-08-23 17:29:15 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-gray-800' >
+       Hola
+      </div>
+    </>
+  )
+}
+
+export default App
